915f518d274ff41fcaa9a06b7352422f31e74185
Guillermo De Fermin <gdefermin@gmail.com>
2019-05-17 19:41:13 +0200

    Initial version

---
+++ src/MyDocument.js
@@ -0,0 +1,327 @@
+import React from 'react'
+import { Document, Page, Text, View } from '@react-pdf/renderer'
+import styled from '@react-pdf/styled-components'
+import python from './assets/python.png'
+import docker from './assets/docker.jpg'
+import firebase from './assets/firebase.png'
+import react from './assets/react.png'
+import redux from './assets/redux.png'
+import javascript from './assets/javascript.png'
+import cpp from './assets/cpp.png'
+import archlinux from './assets/archlinux.png'
+import java from './assets/java.png'
+import raspi from './assets/raspi.png'
+
+const MainPage = styled.Page`
+  flex-direction: row;
+  background-color: white;
+`
+
+const Header = styled.View`
+  background-color: #5b5fab;
+  color: white;
+  padding: 20pt;
+  flex-direction: column;
+  justify-content: flex-start;
+`
+
+const CirclePic = styled.Image`
+  width: 80;
+  height: 80;
+  border-radius: 40;
+  align-self: center;
+  border: 2pt white;
+  margin: 5pt;
+`
+
+const CirclePicSmall = styled.Image`
+  width: 30;
+  height: 30;
+  border-radius: 20;
+  background-color: white;
+  padding: 5pt;
+  margin-right: 5pt;
+  margin-bottom: 5pt;
+`
+
+const PersonalInfo = styled.View`
+  flex-direction: column;
+  font-family: Roboto;
+  justify-content: center;
+  padding: 3pt;
+  margin-bottom: 10pt;
+`
+
+const Title = styled.Text`
+  font-size: 14pt;
+  font-style: italic;
+  margin-bottom: 10pt;
+`
+
+const Description = styled.Text`
+  font-size: 10pt;
+  padding-bottom: 5pt;
+  max-width: 150pt;
+`
+
+const Body = styled.View`
+  padding: 20pt;
+  flex-grow: 1;
+  font-family: Roboto;
+  flex-direction: column;
+`
+
+const SectionTitleText = styled.Text`
+  font-size: 14pt;
+  margin-right: 10pt;
+  color: ${props => props.color || 'black'};
+`
+
+const HorizontalLine = styled.View`
+  flex: 1;
+  background: ${props => props.color || 'black'};
+  height: 1pt;
+`
+
+const SectionTitleWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 10pt;
+  margin-top: 5pt;
+`
+
+const HorizontalLineView = props => (
+  <SectionTitleWrapper>
+    <HorizontalLine color={props.color} />
+  </SectionTitleWrapper>
+)
+
+const SectionTitle = props => (
+  <SectionTitleWrapper>
+    <SectionTitleText color={props.color}>{props.children}</SectionTitleText>
+    <HorizontalLine color={props.color} />
+  </SectionTitleWrapper>
+)
+
+const Company = styled.Text`
+  font-size: 10pt;
+`
+const WorkPeriod = styled.Text`
+  font-weight: lighter;
+  font-size: 8pt;
+`
+const Position = styled.Text`
+  font-weight: bold;
+  font-size: 10pt;
+`
+
+const HeaderTitle = styled.Text`
+  font-size: 12pt;
+  padding-bottom: 10pt;
+`
+
+const Paragraph = styled.Text`
+  margin-top: 7pt;
+  font-size: 10pt;
+  width: 320pt;
+`
+
+const JobDescription = styled.View`
+  padding-bottom: 20pt;
+`
+
+const ProgressBarContainer = styled.View`
+  width: 70pt;
+  height: 12pt;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+const ProgressBarItem = styled.View`
+  width: 10pt;
+  height: 10pt;
+  border-radius: 5pt;
+  background-color: ${props => (props.filled ? 'white' : 'gray')};
+`
+
+const ProgressBar = props => (
+  <ProgressBarContainer>
+    <ProgressBarItem filled={props.count > 0} />
+    <ProgressBarItem filled={props.count > 1} />
+    <ProgressBarItem filled={props.count > 2} />
+    <ProgressBarItem filled={props.count > 3} />
+    <ProgressBarItem filled={props.count > 4} />
+  </ProgressBarContainer>
+)
+
+const Language = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Experience = styled.View`
+  flex-direction: row;
+  width: 150pt;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+`
+
+const MyDocument = () => (
+  <Document>
+    <MainPage size="A4">
+      <Header>
+        <CirclePic src={require('./assets/GuillermoFermin.jpg')} />
+        <PersonalInfo>
+          <Text>Guillermo De Fermín</Text>
+          <Title>Computer Engineer</Title>
+        </PersonalInfo>
+        <PersonalInfo>
+          <Description>📅 June 25th, 1995</Description>
+          <Description>📍 Madrid, Spain</Description>
+          <Description>📧 gdefermin@gmail.com</Description>
+          <Description>📞 +34 696 389 767</Description>
+        </PersonalInfo>
+        <HorizontalLineView color="white" />
+        <PersonalInfo>
+          <HeaderTitle>Languages</HeaderTitle>
+          <Language>
+            <Description> - English</Description>
+            <ProgressBar count={5} />
+          </Language>
+          <Language>
+            <Description> - Spanish</Description>
+            <ProgressBar count={5} />
+          </Language>
+          <Language>
+            <Description> - Italian</Description>
+            <ProgressBar count={4} />
+          </Language>
+          <Language>
+            <Description> - German</Description>
+            <ProgressBar count={3} />
+          </Language>
+          <Language>
+            <Description> - Dutch</Description>
+            <ProgressBar count={1} />
+          </Language>
+          <Language>
+            <Description> - French</Description>
+            <ProgressBar count={1} />
+          </Language>
+        </PersonalInfo>
+        <HorizontalLineView color="white" />
+        <PersonalInfo>
+          <HeaderTitle>Awards / Titles</HeaderTitle>
+          <Description> - Cambridge Certificate in Advanced English (Level C2)</Description>
+          <Description> - 2nd place winner, 2017 Indoor Micro Air Vehicle competition</Description>
+        </PersonalInfo>
+        <HorizontalLineView color="white" />
+        <PersonalInfo>
+          <HeaderTitle>Experience with</HeaderTitle>
+          <Experience>
+            <CirclePicSmall src={python} />
+            <CirclePicSmall src={javascript} />
+            <CirclePicSmall src={react} />
+            <CirclePicSmall src={redux} />
+            <CirclePicSmall src={cpp} />
+            <CirclePicSmall src={docker} />
+            <CirclePicSmall src={archlinux} />
+            <CirclePicSmall src={firebase} />
+            <CirclePicSmall src={java} />
+            <CirclePicSmall src={raspi} />
+          </Experience>
+        </PersonalInfo>
+        <HorizontalLineView color="white" />
+        <PersonalInfo>
+          <HeaderTitle>Hobbies</HeaderTitle>
+          <Description> - Playing guitar and ukulele</Description>
+          <Description> - Travelling</Description>
+          <Description> - Biking, walking and exploring</Description>
+        </PersonalInfo>
+      </Header>
+      <Body>
+        <SectionTitle>Overview</SectionTitle>
+        <JobDescription>
+          <Paragraph>
+            I'm a computer science engineer and I'm passionate about what I do. I like
+            to spend my free time tinkering with computers, and what I love most about
+            my job(s) is when I get to use new technologies or tools. I have
+            experience with many programming languages, but I'm not afraid to program
+            in a language I've never touched before.
+          </Paragraph>
+          <Paragraph>
+            I've worked in many different environments (research, startup, corporate)
+            and this has taught me how to work with many different kinds of people. I
+            really enjoy meeting people from different backgrounds and cultures and
+            learning something new from them.
+          </Paragraph>
+          <Paragraph>
+            I have experience in systems administration (linux), software development
+            in many languages (including Python. Javascript and C++), Scrum/agile
+            methodologies and artificial intelligence.
+          </Paragraph>
+        </JobDescription>
+
+        <SectionTitle>Work Experience</SectionTitle>
+        <JobDescription>
+          <Position>Consultant</Position>
+          <Company>MHP - A Porsche Company</Company>
+          <WorkPeriod>January 2018 - August 2019</WorkPeriod>
+          <Paragraph>
+            I have been working for Porsche as an external consultant, in a
+            cross-brand project with Audi, VW and Porsche. My team's role is to
+            provide a developer tools and a development environment for software
+            developers of the three brands.
+          </Paragraph>
+        </JobDescription>
+
+        <JobDescription>
+          <Position>Co-founder & CTO</Position>
+          <Company>connme. UG</Company>
+          <WorkPeriod>September 2016 - Present</WorkPeriod>
+          <Paragraph>
+            As CTO of connme., I'm responsible of all of the technical aspects of the
+            company. This includes designing and implementing the website and mobile
+            application as well as interviewing new programmers and coordinating and
+            overseeing their work.
+          </Paragraph>
+          <Paragraph>
+            Our efforts so far have led us to release a beta version of the mobile
+            application. We are now searching for investors.
+          </Paragraph>
+        </JobDescription>
+
+        <JobDescription>
+          <Position>Software Engineer</Position>
+          <Company>Computer Vision and Automated Robotics Laboratory (UPM)</Company>
+          <WorkPeriod>May 2016 - September 2017</WorkPeriod>
+          <Paragraph>
+            I designed and implemented 3 core components of the robotics artificial
+            intelligence stack Aerostack. One of the components is responsible for
+            managing a symbolic memory for an unmanned aerial vehicle, while the other
+            two interpret mission plans specified for it.
+          </Paragraph>
+          <Paragraph>
+            I worked closely with other software engineers in my team, as well as with
+            industrial engineers responsible for writing other main components of the
+            stack.
+          </Paragraph>
+          <Paragraph>
+            Our work led us to earn 2nd place at the 2017 Indoor Micro Air Vehicle
+            competition.
+          </Paragraph>
+        </JobDescription>
+
+        <SectionTitle>Education</SectionTitle>
+        <JobDescription>
+          <Position>Bachelor in Computer Science Engineering</Position>
+          <Company>Universidad Politécnica de Madrid</Company>
+          <WorkPeriod>2013 - 2017</WorkPeriod>
+        </JobDescription>
+      </Body>
+    </MainPage>
+  </Document>
+)
+
+export default MyDocument
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { PDFViewer, Font } from '@react-pdf/renderer'
+import MyDocument from './MyDocument'
+
+Font.register({
+  family: 'Roboto',
+  fonts: [
+    { src: require('./assets/fonts/Roboto-Regular.ttf') },
+    { src: require('./assets/fonts/Roboto-Italic.ttf'), fontStyle: 'italic' },
+    { src: require('./assets/fonts/Roboto-Bold.ttf'), fontWeight: 'bold' },
+    { src: require('./assets/fonts/Roboto-Light.ttf'), fontWeight: 'lighter' },
+  ],
+})
+
+Font.registerEmojiSource({
+  format: 'png',
+  url: 'https://twemoji.maxcdn.com/2/72x72/',
+})
+
+const App = () => (
+  <PDFViewer width="100%" height="900">
+    <MyDocument />
+  </PDFViewer>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
